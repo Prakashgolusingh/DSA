5aacc4362d2ac0387c5e678fb572b488664afb4c
Prakash Singh <95623466+Prakashgolusingh@users.noreply.github.com>
2023-05-03 16:11:57 +0530

    Update 1_adjacency_Matrix_List.java

--- Graph/5_basic_problem/1_adjacency_Matrix_List.java
+++ Graph/5_basic_problem/1_adjacency_Matrix_List.java
@@ -1,3 +1,7 @@
+// In java, we genreally have three way to store edges and vertices 
+// 1. using Adjacency matrix e.g ---> G[][];
+// 2. using ArrayList of matrix e.g --> ArrayList<Intger> G[];
+// 3. ArrayList of ArrayList e.g --> ArrayList<ArrayList<Intger>> G;
 import java.util.*;
 public class Main
 {
@@ -5,7 +9,6 @@ public class Main
 	      Scanner scn = new Scanner(System.in);
 	      System.out.print("enter no of vertices and edges with space");
 	      int v =scn.nextInt();
-	      int e = scn.nextInt();
 	      int mtx[][]=new int[v][v];
 		for(int i=0;i<v;i++)
 		{
@@ -14,8 +17,8 @@ public class Main
 		            mtx[i][j]=scn.nextInt();
 		      }
 		}
-		ArrayList<Integer> adj1[] = new ArrayList[v]; //Adjacency matrix
-		ArrayList<ArrayList<Integer>> adj2 = new ArrayList<ArrayList<Integer>>(v); // Adjacency mtx using ArrayList.
+		ArrayList<Integer> adj1[] = new ArrayList[v]; //Adjacency List usng matrix
+		ArrayList<ArrayList<Integer>> adj2 = new ArrayList<ArrayList<Integer>>(v); // Adjacency list using ArrayList.
 		for(int i=0;i<v;i++)
 		{
 		      ArrayList<Integer> arr1 = new ArrayList<Integer>();
@@ -30,12 +33,22 @@ public class Main
 		}
 		for(int i=0;i<v;i++)
 		{
-		      System.out.println(i+" "+adj1[i]);
+		      System.out.print(i);
+		      for ( int j:adj1[i])
+		      {
+		            System.out.print(" "+j);
+		      }
+		      System.out.println();
 		}
-		for(int i=0;i<v;i++)
+		// printing through adj. matrix of ArrayList
+		/*for(int i=0;i<v;i++)
 		{
-		      System.out.println(i+" "+adj2.get(i));
-		}
+		      System.out.print(i);
+		      for( int j:adj2.get(i))
+		      {
+		            System.out.print(" "+j);
+		      }
+		      System.out.println();
+		}*/
 	}
 }
-
