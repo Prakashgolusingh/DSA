7e22752f87d6801046c43769a8600d92bb5b9335
Prakash Singh <prakashgolusingh2020@gmail.com>
2024-03-01 15:54:19 +0530

    Create preak element.java

---
+++ GFG/Binary Search/Peak Element/preak element.java
@@ -0,0 +1,35 @@
+
+/*Complete the function below*/
+
+class Solution
+{
+	// Function to find the peak element
+	// arr[]: input array
+	// n: size of array a[]
+	public int peakElement(int[] arr,int n)
+    {
+        int l = 0;
+        int r = arr.length-1;
+        if(r == 0)
+            return r;
+        if(arr[0]>arr[1])
+            return 0;
+        if(arr[r]>arr[r-1])
+            return r;
+            
+        int mid = 0;
+        while(l<r)
+        {
+            mid = l+(r-l)/2;
+            if(mid == l || r == mid)
+                return mid;
+            if(arr[mid]>=arr[mid-1] && arr[mid]>=arr[mid+1])
+                return mid;
+            if(arr[mid+1]> arr[mid])
+                l = mid;
+            else
+                r = mid;
+        }
+        return -1;
+    }
+}
