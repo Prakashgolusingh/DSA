2472ef43997cb3b5c70a123f305abfe39e194f1f
Prakash Singh <prakashgolusingh2020@gmail.com>
2024-04-14 00:48:53 +0530

    Create solution.java

---
+++ GFG/Bit manipulation/Xoring and Clearing/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void printArr(int n, int arr[]) {
+        for(int i: arr)
+            System.out.print(i+" ");
+        System.out.println();
+    }
+
+    public void setToZero(int n, int arr[]) {
+        for(int i=0; i<n; i++)
+            arr[i] = 0;
+    }
+
+    public void xor1ToN(int n, int arr[]) {
+        for(int i=0; i<n; i++)
+            arr[i] = arr[i]^i;
+    }
+}
