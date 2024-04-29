1da6f4c6e4662796293afc2624b6c31083276fb9
Prakash Singh <prakashgolusingh2020@gmail.com>
2024-04-29 23:41:10 +0530

    Update 198. Optimized.java

--- Leetcode/1-1000/198. House Robber/198. Optimized.java
+++ Leetcode/1-1000/198. House Robber/198. Optimized.java
@@ -8,3 +8,19 @@ class Solution {
         return nums[n-1];
     }
 }
+// just in case we don't want to change source array, we need to create extra space
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length, pre1 = nums[0], pre2 = nums[0];
+        if(n>1)
+            pre2 = Math.max(nums[0], nums[1]);
+        for(int i=2; i<n; i++)
+        {
+            int temp = Math.max(nums[i]+pre1, pre2);
+            pre1 = pre2;
+            pre2 = temp;
+        }
+            
+        return pre2;
+    }
+}
